ad457e72451ee0f9c5b9924d1ed2a7de4b6e96d8
Manasi-1722 <88338443+Manasi-1722@users.noreply.github.com>
2021-08-16 13:00:02 +0530

    Add files via upload

---
+++ List Methods.py
@@ -0,0 +1,48 @@
+# List Methods
+
+colours = ["black", "white", "purple"]
+colours.append("red")
+print(colours)
+
+colours = ["black", "white", "purple"]
+colours.clear()
+print(colours)
+
+colours = ["black", "white", "purple"]
+a = colours.copy()
+print(a)
+
+colours = ["black", "white", "purple"]
+b = colours.count("purple")
+print(b)
+
+colours = ["black","white","purple"]
+cars = ['Ford', 'BMW', 'Volvo']
+colours.extend(cars)
+print(colours)
+
+colours = ["black","white","purple"]
+x = colours.index("black")
+print(x)
+
+colours = ["black","white","purple"]
+colours.insert(1, "red")
+print(colours)
+
+colours = ["black","white","purple"]
+colours.pop(0)
+print(colours)
+
+colours = ["black","white","purple"]
+colours.remove("white")
+print(colours)
+
+colours = ["black","white","purple"]
+colours.reverse()
+print(colours)
+
+colours = ["black","white","purple"]
+colours.sort()
+print(colours)
+
+
